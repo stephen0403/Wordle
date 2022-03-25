8226e5b170ea455001f26d98689b6b0752c7068f
stephen0403 <stephenim83@yahoo.ca>
2022-03-25 10:54:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -40,3 +40,40 @@ pygame.display.update()
 LETTER_X_SPACING = 85
 LETTER_Y_SPACING = 12
 LETTER_SIZE = 75
+
+guesses_count = 0 ## GLOBAL COUNT 
+
+# guesses is a 2D list that will store guesses. A guess will be a list of letters.
+# The list will be iterated through and each letter in each guess will be drawn on the screen.
+guesses = [[]] * 6
+
+current_guess = []
+current_guess_string = ""
+current_letter_bg_x = 110
+
+# Indicators is a list storing all the Indicator object. An indicator is that button thing with all the letters you see.
+indicators = []
+
+game_result = ""
+
+class Letter:
+    def __init__(self, text, bg_position):
+        # Initializes all the variables, including text, color, position, size, etc.
+        self.bg_color = "white"
+        self.text_color = "black"
+        self.bg_position = bg_position
+        self.bg_x = bg_position[0]
+        self.bg_y = bg_position[1]
+        self.bg_rect = (bg_position[0], self.bg_y, LETTER_SIZE, LETTER_SIZE)
+        self.text = text
+        self.text_position = (self.bg_x + 36, self.bg_position[1] + 34)
+        self.text_surface = GUESSED_LETTER_FONT.render(self.text, True,
+                                                       self.text_color)
+        self.text_rect = self.text_surface.get_rect(center=self.text_position)
+
+    def draw(self):
+        # Puts the letter and text on the screen at the desired positions.
+        
+        
+    def delete(self):
+        # Fills the letter's spot with the default square, emptying it.
